2389743b1db8d96d432958800cefd63abf2086f7
Jairo Salazar <jairosalazarvazquez@gmail.com>
2021-03-02 17:10:12 -0600

    Instalación del control de volumen de microfono

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@ chmod a+x getAudio.sh
 mkdir 'FILES'
 chmod a+x getAudio.sh
 
+sudo apt-get update
+sudo apt-get install alsa-base alsa-utils
+sudo apt-get install libasound2-dev
+sudo alsamixer
+sudo alsactl store 1
+
